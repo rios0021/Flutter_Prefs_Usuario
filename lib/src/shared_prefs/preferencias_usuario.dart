5d038be644837577e1acd21f805c81fecdbcb3d3
Eduardo Rios Corlay <rios0021@algonquinlive.com>
2020-05-07 00:24:35 -0400

    App Completada

---
+++ lib/src/shared_prefs/preferencias_usuario.dart
@@ -0,0 +1,60 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class PreferenciasUsuario{
+  
+  static final PreferenciasUsuario _instancia = new PreferenciasUsuario._internal();
+
+  factory PreferenciasUsuario(){
+    return _instancia;
+  }
+  PreferenciasUsuario._internal();
+
+  SharedPreferences _prefs;
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
+  }
+  //Ninguna de estas propiedades se usa
+  // bool _colorSecundario;
+  // int _genero;
+  // String _nombre;
+
+  //GET y SET del Genero
+  get genero{
+    return _prefs.getInt('genero') ?? 1;
+  }
+
+  set genero(int value){
+    _prefs.setInt('genero', value);
+  }
+
+
+  //GET y SET del colorSecundario
+  get colorSecundario{
+    return _prefs.getBool('colorSecundario') ?? false;
+  }
+
+  set colorSecundario(bool value){
+    _prefs.setBool('colorSecundario', value);
+  }
+
+  //GET y SET del nombre
+  get nombreUsuario{
+    return _prefs.getString('nombreUsuario') ?? '';
+  }
+
+  set nombreUsuario(String value){
+    _prefs.setString('nombreUsuario', value);
+  }
+
+  //GET y SET de Ultima Pagina
+  get ultimaPagina{
+    return _prefs.getString('ultimaPagina') ?? 'home';
+  }
+
+  set ultimaPagina(String value){
+    _prefs.setString('ultimaPagina', value);
+  }
+  
+
+}
